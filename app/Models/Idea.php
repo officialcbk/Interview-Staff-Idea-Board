6b84ec67000bdcad327adad34e0c909e785fc560
officialcbk <eeze@academic.rrc.ca>
2026-06-11 12:50:02 -0500

    Idea Model has been completed

---
+++ app/Models/Idea.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\IdeaStatus;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Idea extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => IdeaStatus::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
