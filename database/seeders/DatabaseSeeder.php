b416858c45646cda63215b10ef447d8608c9fd22
officialcbk <eeze@academic.rrc.ca>
2026-06-11 13:15:45 -0500

    Completed Idea seeding and updated Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            TaskSeeder::class,
+            UserSeeder::class,
+            IdeaSeeder::class,
         ]);
     }
 }
--- database/seeders/IdeaSeeder.php
+++ database/seeders/IdeaSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Idea;
 use Illuminate\Database\Seeder;
 
 class IdeaSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Idea::create([
+            'user_id' => 1,
+            'title' => 'Improve member onboarding process',
+            'body' => 'We should streamline the onboarding process by creating a digital checklist for new members so nothing gets missed during account setup.',
+            'status' => 'under_review',
+        ]);
+
+        Idea::create([
+            'user_id' => 2,
+            'title' => 'Add a mobile app notification system',
+            'body' => 'Members frequently miss important alerts. A push notification system in the mobile app would improve engagement and reduce missed payments.',
+            'status' => 'planned',
+        ]);
+
+        Idea::create([
+            'user_id' => 1,
+            'title' => 'Saturday branch hours extension',
+            'body' => 'Many members work weekdays and struggle to visit during regular hours. Extending Saturday hours to 4pm would significantly improve accessibility.',
+            'status' => 'under_review',
+        ]);
     }
 }
