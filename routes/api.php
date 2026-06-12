f731d691939e1cd24eed9143711725515063790f
officialcbk <eeze@academic.rrc.ca>
2026-06-12 12:08:18 -0500

    Ideas routes registered

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\IdeaController;
 
 Route::apiResource('tasks', TaskController::class);
-
+Route::apiResource('ideas', IdeaController::class);
