9abe0a6359b3b04ad70a63f50cd2af718ca7de74
officialcbk <eeze@academic.rrc.ca>
2026-06-12 12:07:50 -0500

    Ideas controller and API

---
+++ app/Http/Resources/IdeaResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+
+class IdeaResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'status' => $this->status->value,
+            'status_label' => $this->status->label(),
+            'vote_count' => 0,
+            'comment_count' => 0,
+            'author' => $this->user->name,
+            'created_at' => $this->created_at->toDateTimeString(),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\IdeaController;
 
 Route::apiResource('tasks', TaskController::class);
+
