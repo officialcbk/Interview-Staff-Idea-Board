e6adc37805fcb4c58eace7f01966caebddbf8526
officialcbk <eeze@academic.rrc.ca>
2026-06-14 06:17:25 -0500

    User authorization started, User controller completed

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::select('id', 'name', 'role')->get();
+    }
+}
