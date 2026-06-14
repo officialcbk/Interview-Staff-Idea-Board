b3437cbc40d7980b1f6366899dfd07c6cc9bcbeb
officialcbk <eeze@academic.rrc.ca>
2026-06-14 06:19:29 -0500

    User route completed

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\IdeaController;
+use App\Http\Controllers\Api\UserController;
 
 Route::apiResource('tasks', TaskController::class);
 Route::apiResource('ideas', IdeaController::class);
+Route::get('/users', [UserController::class, 'index']);
