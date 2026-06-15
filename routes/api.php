f6c3e8175069c67664efe175a4b2fdb54534e252
officialcbk <eeze@academic.rrc.ca>
2026-06-14 21:06:06 -0500

    Added the votes route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\IdeaController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VoteController;
 
 Route::apiResource('tasks', TaskController::class);
 Route::apiResource('ideas', IdeaController::class);
 Route::apiResource('users', UserController::class);
+Route::post('/ideas/{idea}/vote', [VoteController::class, 'toggle']);
